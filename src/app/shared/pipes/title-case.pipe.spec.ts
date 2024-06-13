8c80aa29297b7e1d5ff21618b6340757f06e638a
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2024-06-13 17:42:27 +0200

    commit #3

---
+++ src/app/shared/pipes/title-case.pipe.spec.ts
@@ -0,0 +1,11 @@
+import { TitleCasePipe } from './title-case.pipe';
+
+describe('TitleCasePipe', () => {
+  // This pipe is a pure, stateless function so no need for BeforeEach
+  const pipe = new TitleCasePipe();
+  it('transforms "the dark night rises" to "The Dark Knight Rises"', () => {
+    expect(pipe.transform('the dark knight rises')).toBe(
+      'The Dark Knight Rises'
+    );
+  });
+});
